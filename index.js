c6c0bd8066740e4518dc8e46d7514efd90312745
HENRY <ebongemmanuel197@gmail.com>
2023-06-03 11:39:40 +0100

    third-commit

--- index.js
+++ index.js
@@ -5,12 +5,52 @@ let form = document.querySelector("form")
 let modal = document.querySelector(".modal")
 let container = document.querySelector(".container")
 let dissBtn = document.getElementById("diss-btn")
+
+
+
+subscribeBtn.addEventListener("click", function(){
+    // Regular expression pattern to match the email address
+    let pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+    // Use the test() method to check if the email matches the pattern
+    if (pattern.test(userEmail.value)) {
+      errorEl.textContent = ""
+    } else {
+      errorEl.textContent = "Wrong email Format"
+    userEmail.style.borderColor = "hsl(4, 100%, 67%)"
+    let style = document.createElement("style");
+style.innerHTML = "::placeholder { color: hsl(4, 100%, 67%); }";
+document.head.appendChild(style);
+userEmail.style.backgroundColor = "hsl(4, 48%, 87%)"
+}
+})
+
+  
+  
+    
+    
+     
+  
+  // Example usage
+
+ 
+
 form.addEventListener("submit", (e) => {
+    e.preventDefault()
+   
+
     let messages = []
-   if(userEmail.value === "" || userEmail.value == null){
-    messages.push("Valid email required")   
-   } 
-   e.preventDefault()
+
+
+    
+   if(userEmail.value.length === 0 ){
+    messages.push ("Valid email required" ) 
+    
+   }else if (userEmail.value.length > 0 && userEmail.value.length < 10){
+    messages.push("Email too short")
+   }
+    
+  
     if (messages.length > 0){
         e.preventDefault()
         errorEl.textContent = messages.join(", ")
@@ -22,12 +62,14 @@ userEmail.style.backgroundColor = "hsl(4, 48%, 87%)"
 
 
     }
-    if (messages.length <= 0){
+
+    if (messages.length === 0){
         modal.style.display = "inline-block"
         container.style.display = "none"
         let currentEmail = document.getElementById("confirm-email");
         currentEmail.textContent = userEmail.value
     }
+    
 })
 
 dissBtn.addEventListener("click", function(){
@@ -41,4 +83,5 @@ userEmail.style.backgroundColor = ""
 userEmail.style.borderColor = ""
 errorEl.textContent = ""
 
-})
+})  
+
